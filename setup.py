61ee4adc64368203c0745937e2f4ef504717bd2f
Sylvain Quoilin <sylvain.quoilin@ec.europa.eu>
2017-12-18 17:02:57 +0100

    added data files together with the library in the installation

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+import os
 
 
 # Sets the __version__ variable
 __version__ = None
 exec(open('DispaSET/_version.py').read())
+
+def recursive_listdir(folder):
+    '''
+    Generates a list of all files in a a specified directory
+    '''
+    return [(dp,[os.path.join(dp, f)]) for dp, dn, fn in os.walk(os.path.expanduser(folder)) for f in fn]
+
+def define_data_files():
+    list_files = []
+    for folder in ['ConfigFile','Database','Externals']:
+        list_files += recursive_listdir(folder)
+    return list_files
+        
 
 setup(
     name='Dispa-SET',
@@ -18,6 +32,7 @@ setup(
     download_url='http://www.dispaset.eu/en/latest/releases.html',
     packages=find_packages(),
     package_data={'DispaSET': ['config/*.yaml','GAMS/*']},
+    data_files=define_data_files(),
     install_requires=[
         "click >= 3.3",
         "numpy >= 1.12",
