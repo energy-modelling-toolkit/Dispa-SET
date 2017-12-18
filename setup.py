9c6cf1d94dc3a7d34dca0a33d65a3ec608b55648
Sylvain Quoilin <sylvain.quoilin@ec.europa.eu>
2017-12-18 15:42:24 +0100

    hard coding packages to include in order to avoid import pathlib

--- setup.py
+++ setup.py
@@ -1,30 +1,11 @@
 #!/usr/bin/env python
 
-from pathlib import Path
 from setuptools import setup, find_packages
 
 
 # Sets the __version__ variable
 __version__ = None
 exec(open('DispaSET/_version.py').read())
-
-
-def get_subdirs(path, glob_string):
-    return [
-        i.relative_to(str(path) + '/')
-        for i in path.glob(glob_string) if i.is_dir()
-    ]
-
-
-def find_package_data():
-    """Returns a list of found directories with package data files"""
-    path = Path('./DispaSET')
-    package_data = ['config/*.yaml','GAMS/*']
-    for test_case_dir in get_subdirs(path, 'test/*'):
-        package_data.append(str(test_case_dir) + '/*.csv')
-    print(package_data)
-    return package_data
-
 
 setup(
     name='Dispa-SET',
@@ -36,7 +17,7 @@ setup(
     url='http://www.dispaset.eu',
     download_url='http://www.dispaset.eu/en/latest/releases.html',
     packages=find_packages(),
-    package_data={'DispaSET': find_package_data()},
+    package_data={'DispaSET': ['config/*.yaml','GAMS/*']},
     install_requires=[
         "click >= 3.3",
         "numpy >= 1.12",
