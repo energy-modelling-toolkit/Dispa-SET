87edb1ed44f7364be3103c35bb024359a2d35328
Damien <damien.corral@mines-paristech.fr>
2020-02-20 15:46:43 +0100

    Remove useless _release.py

--- dispaset/_release.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-# edit manually when a new release is created (git tag -a)
-# fallback version when package is installed from git archive
-# for backward compatibility only
-release = '2.3'
