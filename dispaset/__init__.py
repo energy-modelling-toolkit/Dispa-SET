60bca269a362f48f2c6abfb181d657fd559beec0
Damien Corral <47735753+corralien@users.noreply.github.com>
2020-02-21 18:11:03 +0100

    Fix runtime version (#67)
    * Fix runtime version
    
    * Use logging instead of warning
    
    * Filter warnings from setuptools_scm
    
    * better warning
    
    Co-authored-by: Konstantinos Kavvadias <kavvkon@gmail.com>

--- dispaset/__init__.py
+++ dispaset/__init__.py
@@ -1,18 +1,7 @@
 import logging.config
 import os
 
-# Sets the __version__ variable
 from .common import commons
-
-try:
-    from setuptools_scm import get_version
-    version = get_version(version_scheme='post-release',
-                          local_scheme=lambda version: version.format_choice("" if version.exact else "+{node}", "+dirty"),
-                          root='..', relative_to=__file__)
-except (ImportError, LookupError):
-    import pkg_resources
-    version = pkg_resources.get_distribution(__package__).version
-__version__ = version
 
 # Logging: # TODO: Parametrize in dispacli or external config
 _LOGCONFIG = {
@@ -53,6 +42,34 @@ _LOGCONFIG = {
      }
 }
 
+# Setting logging configuration:
+try:
+    logging.config.dictConfig(_LOGCONFIG)
+except Exception:
+    # if it didn't work, it might be due to ipython messing with the output
+    # typical error: Unable to configure handler 'console': IOStream has no fileno
+    # try without console output:
+    print('WARNING: the colored console output is failing (possibly because of ipython). Switching to monochromatic output')
+    _LOGCONFIG['handlers']['console']['class'] = "logging.StreamHandler"
+    logging.config.dictConfig(_LOGCONFIG)
+
+# Sets the __version__ variable
+try:
+    from setuptools_scm import get_version
+    import warnings
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        version = get_version(version_scheme='post-release',
+                              local_scheme=lambda version: version.format_choice("" if version.exact else "+{node}", "+dirty"),
+                              root='..', relative_to=__file__)
+except (ImportError, LookupError):
+    try:
+        from pkg_resources import get_distribution, DistributionNotFound
+        version = get_distribution(__package__).version
+    except DistributionNotFound:
+        logging.warning("Unable to detect version, most probably because you did not install it properly. To avoid further errors, please install it by running 'pip install -e .'.")
+        version = 'N/A'
+__version__ = version
 
 # Importing the main Dispa-SET functions so that they can be called with "ds.function"
 from .preprocessing.data_handler import load_config_excel, load_config_yaml, load_config, export_yaml_config
@@ -73,14 +90,3 @@ for filename in (f for f in os.listdir('.') if f.endswith('.dispa.log')):
         os.remove(filename)
     except OSError:
         print ('Could not erase previous log file ' + filename)
-
-# Setting logging configuration:
-try:
-    logging.config.dictConfig(_LOGCONFIG)
-except Exception:
-    # if it didn't work, it might be due to ipython messing with the output
-    # typical error: Unable to configure handler 'console': IOStream has no fileno
-    # try without console output:
-    print('WARNING: the colored console output is failing (possibly because of ipython). Switching to monochromatic output')
-    _LOGCONFIG['handlers']['console']['class'] = "logging.StreamHandler"
-    logging.config.dictConfig(_LOGCONFIG)
