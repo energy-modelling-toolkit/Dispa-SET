6f2ac9af82e90c8ffe774b98d4dda24e3d41afd7
Matija Pavicevic <matija.pavicevic@gmail.com>
2020-09-15 19:21:30 +0200

    Improved utils.py testing and debugging of utils.py functions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,14 @@
+import itertools
 import os
 import pytest
 import dispaset as ds
 import numpy as np
+import pandas as pd
 
-from dispaset.preprocessing.utils import pd_timestep, _mylogspace, _find_nearest, _flatten_list
-# TODO: test following functions: incidence_matrix, interconnections, _get_index, group_plants, clustering, adjust_storage, adjust_capacity
+from dispaset.preprocessing.utils import pd_timestep, _mylogspace, _find_nearest, _flatten_list, _split_list, \
+    _merge_two_dicts, create_agg_dict
+
+# TODO: test following functions: incidence_matrix, interconnections, group_plants, _get_index, clustering, adjust_storage, adjust_capacity
 
 
 @pytest.mark.parametrize('TIMESTEP', [1, 0.25, 24, '24'])
@@ -32,3 +36,37 @@ def test_find_nearest(array, value, expected):
 def test_flatten_list(list, expected):
     result = _flatten_list(list) == expected
     assert result
+
+
+@pytest.mark.parametrize('list, expected', [(['Split', 'me'], 'Split - me')])
+def test_split_list(list, expected):
+    result = _split_list(list) == expected
+    assert result
+
+
+@pytest.mark.parametrize('x,y,expected',
+                         [({'a': 1, 'b': 'c'}, {'d': 2, 'e': 'f'}, {'a': 1, 'b': 'c', 'd': 2, 'e': 'f'})])
+def test_merge_two_dict(x, y, expected):
+    result = _merge_two_dicts(x, y) == expected
+    assert result
+
+
+CLUSTERING = ['LP clustered', 'MILP', 'Standard', 'Integer clustering', 'No Clustering']
+PLANTS = [os.path.abspath('./tests/dummy_data/buggy_data/Clustering.csv')]
+cartesian = [elem for elem in itertools.product(*[CLUSTERING, PLANTS])]
+
+
+@pytest.fixture(scope='module', params=cartesian, name='clustering')
+def read_plants(request):
+    method = request.param[0]
+    plants = pd.read_csv(request.param[1])
+    plants.set_index('Unit', inplace=True, drop=False)
+    return method, plants
+
+
+def test_agg_dict(clustering):
+    if clustering[0] == 'No Clustering':
+        with pytest.raises(SystemExit):
+            create_agg_dict(clustering[1], clustering[0])
+    else:
+        create_agg_dict(clustering[1], clustering[0])
