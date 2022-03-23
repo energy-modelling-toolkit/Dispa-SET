0a660338dbbe44743a3c0b60fba75c27441a42d2
Sylvain <squoilin@ulg.ac.be>
2022-03-23 15:23:23 +0100

    troubleshooting gams path handling

--- dispaset/misc/gms_handler.py
+++ dispaset/misc/gms_handler.py
@@ -9,8 +9,8 @@ from .str_handler import force_str
 def solve_high_level(gams_folder, sim_folder, gams_file='UCM_h.gms', result_file='Results.gdx', output_lst=False):
     """Use higher level apis to run GAMSy"""
     # create GAMS workspace:
-    if isinstance(gams_folder, bytes):
-        gams_folder = gams_folder.decode()
+    gams_folder = force_str(gams_folder)
+    gams_folder = b'/home/sylvain/progs/GAMS/gams37.1_linux_x64_64_sfx/'
     try:
         from gams import GamsWorkspace
         ws = GamsWorkspace(system_directory=str(gams_folder), debug=3)
