e14bb2c0706cfe93b43152fbadafa016dba9ab1e
Matija Pavicevic <31565754+MPavicevic@users.noreply.github.com>
2022-04-23 14:43:05 +0200

    fix(data_handler): drop duplicate values when reading timeseries with different datetime index compared to the ones in the config file

--- dispaset/preprocessing/data_handler.py
+++ dispaset/preprocessing/data_handler.py
@@ -430,6 +430,7 @@ def load_time_series(config, path, header='infer'):
                 mask = mask.groupby(mask.index.hour).mean()
                 time = pd.date_range(str(config['idx'][0].year) + '-2-29', periods=24, freq='H')
                 mask = mask.set_index(time)
+                data = data[~data.index.duplicated(keep='last')]
                 data = data.reindex(config['idx'])
                 data.update(mask)
         # recompute common index entries, and check again:
