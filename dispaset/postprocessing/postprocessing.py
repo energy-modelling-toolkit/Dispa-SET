fix(all_bs): h2 sector removed, can be entirely modeled with boundary sector units

dispaset/postprocessing/postprocessing.py
@@ -530,7 +530,6 @@ def CostExPost(inputs, results):
              +sum(l,PriceTransmission(l,i)*Flow(l,i))
              +sum(n,CostLoadShedding(n,i)*ShedLoad(n,i))
              +sum(chp, CostHeatSlack(chp,i) * HeatSlack(chp,i))
-             +sum(p2h2, CostH2Slack(p2h2,i) * StorageSlack(p2h2,i))
              +sum(chp, CostVariable(chp,i) * CHPPowerLossFactor(chp) * Heat(chp,i))
              +Config("ValueOfLostLoad","val")*(sum(n,LL_MaxPower(n,i)+LL_MinPower(n,i)))
              +0.8*Config("ValueOfLostLoad","val")*(sum(n,LL_2U(n,i)+LL_2D(n,i)+LL_3U(n,i)))
@@ -629,11 +628,11 @@ def CostExPost(inputs, results):
             CostHeat[u] = dfin['CostVariable'][u].fillna(0) * results['OutputHeat'][u].fillna(0)
     costs['CostHeat'] = CostHeat.sum(axis=1).fillna(0)
 
-    # %% Cost H2:
-    CostH2 = pd.DataFrame(index=costs.index, columns=dfin['CostH2Slack'].columns)
-    for u in dfin['CostH2Slack'].columns:
-        CostH2[u] = dfin['CostH2Slack'][u].fillna(0) * results['OutputStorageSlack'][u].fillna(0)
-    costs['CostH2Slack'] = CostH2.fillna(0).sum(axis=1)
+    # # %% Cost H2:
+    # CostH2 = pd.DataFrame(index=costs.index, columns=dfin['CostH2Slack'].columns)
+    # for u in dfin['CostH2Slack'].columns:
+    #     CostH2[u] = dfin['CostH2Slack'][u].fillna(0) * results['OutputStorageSlack'][u].fillna(0)
+    # costs['CostH2Slack'] = CostH2.fillna(0).sum(axis=1)
 
     # %% Lost loads:
     # NB: the value of lost load is currently hard coded. This will have to be updated

dispaset/preprocessing/preprocessing.py
@@ -45,12 +45,12 @@ def build_simulation(config, mts_plot=None, MTSTimeStep=24):
         logging.info('Simulation without mid therm scheduling')
         SimData = build_single_run(config)
     else:
-        if config['H2FlexibleDemand'] != '':
-            [new_profiles, new_PtLDemand] = mid_term_scheduling(config, mts_plot=mts_plot, TimeStep=MTSTimeStep)
-            # Build simulation data with new profiles
-            logging.info('\n\nBuilding final simulation\n')
-            SimData = build_single_run(config, profiles=new_profiles, PtLDemand=new_PtLDemand)
-        elif config['BSFlexibleDemand'] != '':
+        # if config['H2FlexibleDemand'] != '':
+        #     [new_profiles, new_PtLDemand] = mid_term_scheduling(config, mts_plot=mts_plot, TimeStep=MTSTimeStep)
+        #     # Build simulation data with new profiles
+        #     logging.info('\n\nBuilding final simulation\n')
+        #     SimData = build_single_run(config, profiles=new_profiles, PtLDemand=new_PtLDemand)
+        if config['BSFlexibleDemand'] != '':
             [new_profiles, new_BSFlexDemand] = mid_term_scheduling(config, mts_plot=mts_plot, TimeStep=MTSTimeStep)
             # Build simulation data with new profiles
             logging.info('\n\nBuilding final simulation\n')
@@ -202,25 +202,25 @@ def mid_term_scheduling(config, TimeStep=None, mts_plot=None):
                     sys.exit(1)
                 for u_old in units.loc[u, 'FormerUnits']:
                     profiles[u_old] = temp_results[c]['OutputStorageLevel'][u].values
-            if config['H2FlexibleDemand'] != '':
-                if 'OutputPtLDemand' not in temp_results[c]:
-                    logging.critical('PtL demand in zone ' + c + ' was not computed')
-                    sys.exit(0)
-                elif len(temp_results[c]['OutputPtLDemand']) > len(idx):
-                    logging.critical('The number of time steps in the mid-term simulation results (' + str(
-                        len(temp_results[c]['OutputPtLDemand'])) +
-                                     ') does not match the length of the index (' + str(len(idx)) + ')')
-                    sys.exit(0)
-                elif len(temp_results[c]['OutputPtLDemand']) < len(idx):
-                    temp_results[c]['OutputPtLDemand'] = temp_results[c]['OutputPtLDemand'].reindex(
-                        range(1, len(idx) + 1)).fillna(0)
-                # for u in temp_results[c]['OutputPtLDemand']:
-                #     if u not in units.index:
-                #         logging.critical('Unit "' + u + '" is reported in the PtL demand of the result file but '
-                #                                         'does not appear in the units table')
-                #         sys.exit(1)
-                #     for u_old in units.loc[u, 'FormerUnits']:
-                #         PtLDemand[u_old] = temp_results[c]['OutputPtLDemand'][u].values
+            # if config['H2FlexibleDemand'] != '':
+            #     if 'OutputPtLDemand' not in temp_results[c]:
+            #         logging.critical('PtL demand in zone ' + c + ' was not computed')
+            #         sys.exit(0)
+            #     elif len(temp_results[c]['OutputPtLDemand']) > len(idx):
+            #         logging.critical('The number of time steps in the mid-term simulation results (' + str(
+            #             len(temp_results[c]['OutputPtLDemand'])) +
+            #                          ') does not match the length of the index (' + str(len(idx)) + ')')
+            #         sys.exit(0)
+            #     elif len(temp_results[c]['OutputPtLDemand']) < len(idx):
+            #         temp_results[c]['OutputPtLDemand'] = temp_results[c]['OutputPtLDemand'].reindex(
+            #             range(1, len(idx) + 1)).fillna(0)
+            # for u in temp_results[c]['OutputPtLDemand']:
+            #     if u not in units.index:
+            #         logging.critical('Unit "' + u + '" is reported in the PtL demand of the result file but '
+            #                                         'does not appear in the units table')
+            #         sys.exit(1)
+            #     for u_old in units.loc[u, 'FormerUnits']:
+            #         PtLDemand[u_old] = temp_results[c]['OutputPtLDemand'][u].values
             if config['BSFlexibleDemand'] != '':
                 if 'OutputBSFlexDemand' not in temp_results[c]:
                     logging.critical('BS Flex demand in zone ' + c + ' was not computed')
@@ -258,14 +258,14 @@ def mid_term_scheduling(config, TimeStep=None, mts_plot=None):
         else:
             profiles = temp_results['OutputStorageLevel'].set_index(idx)
 
-        if config['H2FlexibleDemand'] != '':
-            if 'OutputPtLDemand' not in temp_results:
-                logging.critical('PtL Demand in the selected region was not computed')
-                sys.exit(0)
-            if len(temp_results['OutputPtLDemand']) < len(idx):
-                PtLDemand = temp_results['OutputPtLDemand'].reindex(range(1, len(idx) + 1)).fillna(0).set_index(idx)
-            else:
-                PtLDemand = temp_results['OutputPtLDemand'].set_index(idx)
+        # if config['H2FlexibleDemand'] != '':
+        #     if 'OutputPtLDemand' not in temp_results:
+        #         logging.critical('PtL Demand in the selected region was not computed')
+        #         sys.exit(0)
+        #     if len(temp_results['OutputPtLDemand']) < len(idx):
+        #         PtLDemand = temp_results['OutputPtLDemand'].reindex(range(1, len(idx) + 1)).fillna(0).set_index(idx)
+        #     else:
+        #         PtLDemand = temp_results['OutputPtLDemand'].set_index(idx)
 
         if config['BSFlexibleDemand'] != '':
             if 'OutputBSFlexDemand' not in temp_results:
@@ -304,8 +304,8 @@ def mid_term_scheduling(config, TimeStep=None, mts_plot=None):
 
     # replace all 1.000000e+300 values by nan since they correspond to undefined in GAMS:
     profiles[profiles >= 1E300] = np.nan
-    if config['H2FlexibleDemand'] != '':
-        PtLDemand[PtLDemand >= 1E300] = np.nan
+    # if config['H2FlexibleDemand'] != '':
+    #     PtLDemand[PtLDemand >= 1E300] = np.nan
 
     if config['BSFlexibleDemand'] != '':
         BSFlexDemand[BSFlexDemand >= 1E300] = np.nan
@@ -324,16 +324,16 @@ def mid_term_scheduling(config, TimeStep=None, mts_plot=None):
 
     if config['SimulationTimeStep'] != temp_config['SimulationTimeStep']:
         profiles = profiles.reindex(idx_long, method='nearest')
-        if config['H2FlexibleDemand'] != '':
-            PtLDemand = PtLDemand.resample(pd_timestep(config['SimulationTimeStep'])).pad()
-            PtLDemand = PtLDemand.iloc[0:len(idx_long), :]
+        # if config['H2FlexibleDemand'] != '':
+        #     PtLDemand = PtLDemand.resample(pd_timestep(config['SimulationTimeStep'])).pad()
+        #     PtLDemand = PtLDemand.iloc[0:len(idx_long), :]
         if config['BSFlexibleDemand'] != '':
             BSFlexDemand = BSFlexDemand.resample(pd_timestep(config['SimulationTimeStep'])).pad()
             BSFlexDemand = BSFlexDemand.iloc[0:len(idx_long), :]
     pickle.dump(profiles, open(os.path.join(config['SimulationDirectory'], "temp_profiles.p"), "wb"))
-    if config['H2FlexibleDemand'] != '':
-        return profiles, PtLDemand
-    elif config['BSFlexibleDemand'] != '':
+    # if config['H2FlexibleDemand'] != '':
+    #     return profiles, PtLDemand
+    if config['BSFlexibleDemand'] != '':
         return profiles, BSFlexDemand
     else:
         return profiles

dispaset/preprocessing/utils.py
@@ -374,9 +374,9 @@ def group_plants(plants, method, df_grouped=False, group_list=None):
 
     """
     # Definition of the merged power plants dataframe:
-    if (group_list is None) and ((plants['Zone_th'] != np.nan).all()) and ((plants['Zone_h2'] != np.nan).all()):
-        group_list = ['Zone', 'Zone_th', 'Zone_h2', 'Technology', 'Fuel', 'CHPType']
-    elif (group_list is None) and ((plants['Zone_th'] != np.nan).all()):
+    # if (group_list is None) and ((plants['Zone_th'] != np.nan).all()) and ((plants['Zone_h2'] != np.nan).all()):
+    #     group_list = ['Zone', 'Zone_th', 'Zone_h2', 'Technology', 'Fuel', 'CHPType']
+    if (group_list is None) and ((plants['Zone_th'] != np.nan).all()):
         group_list = ['Zone', 'Zone_th', 'Technology', 'Fuel', 'CHPType']
     else:
         group_list = ['Zone', 'Technology', 'Fuel', 'CHPType']